bde4002f6dc92f6e564d0943ec9683a653f7cb7a
eddievasquez2110 <eddieomarvasquez@gmail.com>
2024-01-20 15:16:05 -0500

    scroll

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Resultado } from 'src/app/interfaces/pokeapi';
 import { PokemonService } from 'src/app/services/pokemon.service';
 
@@ -10,15 +10,30 @@ import { PokemonService } from 'src/app/services/pokemon.service';
 export class HomeComponent implements OnInit{
 
   constructor(private pokemonService : PokemonService){}
+  @ViewChild('tarjetas') tarjetasElement!: ElementRef;
 
   listaPokemon:Resultado[] = []
+
+  pagina:number = 1;
+  cargando:boolean = false;
 
   ngOnInit(): void {
     this.cargarLista();
   }
 
   async cargarLista(){
-    this.listaPokemon = [...this.listaPokemon, ...await this.pokemonService.getByPage()];
-    console.log(this.listaPokemon);
+    this.cargando=true;
+    this.listaPokemon = [...this.listaPokemon, ...await this.pokemonService.getByPage(this.pagina)];
+    this.cargando=false;
+    this.pagina++;
+  }
+
+  onScroll(e:any){
+    if(this.cargando) return;
+    if(Math.round(
+      this.tarjetasElement.nativeElement.clientHeight + this.tarjetasElement.nativeElement.scrollTop
+    ) === e.srcElement.scrollHeight){
+      this.cargarLista();
+    }
   }
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -8,8 +8,10 @@ export class PokemonService {
 
   constructor() { }
 
-  async getByPage():Promise<Resultado[]>{
-    const res = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0");
+  async getByPage(page:number, size:number = 40):Promise<Resultado[]>{
+    if(page > 5) return [];
+    const offset = size*(page-1);
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${size}&offset=${offset}`);
     const resJson = await res.json();
     if (resJson.results.length > 0) return resJson.results;
     return [];
